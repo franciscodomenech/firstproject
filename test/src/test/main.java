7a1290dfc6191bf0ffe9f71f98ea7cb85be7fe3e
lolmeda <lolmeda@serbatic-03863.VASS.INET>
2017-08-29 13:40:48 +0200

    prueba numero 2

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -3,6 +3,9 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
+		
+		boolean esJavi=false;
+		
 		System.out.println("Hola mundo!!!!!");
 		System.out.println("variante");
 		System.out.println("Wasa!!!!");
