e3610ac17c4cfac60baab7b99f7b2ce38d5c5716
aballesteros <aballesteros@serbatic-03868.VASS.INET>
2017-08-29 15:09:49 +0200

    ese daniel

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -3,12 +3,8 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!!!");
-		System.out.println("variante");
-		System.out.println("variante");
-		System.out.println("variante");
-		System.out.println("variante");
-		System.out.println("variante");
+		System.out.println("daniel eres un pachacho");
+
 	}
 
 }
