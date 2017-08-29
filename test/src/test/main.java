143494442488035fd9611b5766a1f1193d56976c
rbustamante <rbustamante@serbatic-03865.VASS.INET>
2017-08-29 13:53:27 +0200

    wsdfghuio

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -4,7 +4,6 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!!!");
-		System.out.println("variante");
 		System.out.print("nueva rama3");
 		System.out.print("nueva rama4");
 	}
