c2dbaebd548b4fe9b7e18e92ad42e32f90937928
jamoreno <jamoreno@serbatic-03869.VASS.INET>
2017-08-29 13:05:05 +0200

    Signed-off-by: jamoreno <jamoreno@serbatic-03869.VASS.INET>

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!!!");
 		System.out.println("variante");
-		System.out.println("n rama nueva4");
+		System.out.println("n rama nueva");
 		
 	}
 
