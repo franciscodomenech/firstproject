a19e7b8ba6a7167b1855b5f1fc48af72aef44a8e
raragon <raragon@serbatic-03875.VASS.INET>
2017-08-29 13:36:11 +0200

    prueba de rubo

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!!!");
 		System.out.println("variante");
+		System.out.println("Viva España!!!");
+		System.out.println("OTR<A PRUEBASDlkjsaasdf");
 	}
 
 }
