8a4682d32b33930fe7a1b76dea3bdde9d553d9b8
dcastillejo <dcastillejo@serbatic-03866.VASS.INET>
2017-08-29 15:31:30 +0200

    hhh

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!!!");
 		System.out.print("nueva rama30");
-		System.out.print("nueva ramja5");
+		System.out.print("nueva ramjao5");
 	}
 
 }
