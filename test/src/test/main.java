141ec8aa9b4fabe1113525d5652c26b4d27b980c
Jccle90 <31438144+Jccle90@users.noreply.github.com>
2017-08-29 12:54:01 +0200

    Ultima

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -3,7 +3,9 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!!!");
+		System.out.println("Hola mundo, JJJJ");
+		System.out.println("variante");
+		
 		System.out.println("variante");
 	}
 
