d7d8748f29e624636a91647cf6d25473e5fac2a3
rbustamante <rbustamante@serbatic-03865.VASS.INET>
2017-08-29 13:46:02 +0200

    uiuoupupoiuio

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -5,7 +5,6 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!!!");
 		System.out.println("variante");
-		System.out.print("nueva rama2");
 		System.out.print("nueva rama3");
 		System.out.print("nueva rama4");
 	}
