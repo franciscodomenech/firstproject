f4e1018b38ca8cdd54373a7659d862ec60ef3c16
aballesteros <aballesteros@serbatic-03868.VASS.INET>
2017-08-29 13:13:29 +0200

    meeee

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -7,6 +7,8 @@ public class main {
 		System.out.println("varianteSSSSssssssSs");
 		System.out.println("varianteSSSSssssssSs");
 		System.out.println("varianteSSSSssssssSs");
+		System.out.println("varianteSSSSssssssSs");
+		System.out.println("varianteSSSSssssssSs");
 	}
 
 }
